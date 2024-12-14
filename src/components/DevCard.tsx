c5ff8f042c5f144321e4f08af75980f143cffc93
BodartA <a.bodart23@gmail.com>
2024-12-14 15:20:58 +0100

    Project setup, ready to work on the Devcard

---
+++ src/components/DevCard.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const DevCard = () => {
+  return (
+    <>
+      <div className='h-[300px] w-[700px] flex justify-center items-center drop-shadow-2xl'>
+
+        <div className=' h-full w-2/3 flex bg-zinc-50 rounded-l-3xl rounded-r-lg py-6 pl-6 pr-14'>
+          <div className=' size-full bg-emerald-600'>
+
+          </div>
+
+        </div>
+
+        <div className=' h-full w-1/3 bg-zinc-50 rounded-r-3xl rounded-l-lg'>
+
+        </div>
+
+      </div >
+    </>
+  );
+};
+
+export default DevCard;
