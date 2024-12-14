68404a366bce1a1e2cad54668cab7ea0bfb3de59
BodartA <a.bodart23@gmail.com>
2024-12-14 16:08:06 +0100

    Commit before trying to implement barecode generator

--- src/components/DevCard.tsx
+++ src/components/DevCard.tsx
@@ -1,19 +1,40 @@
 import React from 'react';
+import { Twitter, Github, Linkedin } from 'lucide-react'
 
 const DevCard = () => {
   return (
     <>
-      <div className='h-[300px] w-[700px] flex justify-center items-center drop-shadow-2xl'>
+      <div className='h-[280px] w-[800px] flex justify-center items-center drop-shadow-2xl'>
 
-        <div className=' h-full w-2/3 flex bg-zinc-50 rounded-l-3xl rounded-r-lg py-6 pl-6 pr-14'>
+        <div className=' h-full w-[600px] flex bg-zinc-50 rounded-l-3xl rounded-r-lg py-6 pl-6 pr-14'>
           <div className=' size-full bg-emerald-600'>
 
           </div>
 
         </div>
 
-        <div className=' h-full w-1/3 bg-zinc-50 rounded-r-3xl rounded-l-lg'>
+        <div className=' flex h-full w-[200px] bg-zinc-50 rounded-r-3xl rounded-l-lg'>
 
+          <div className=' h-full w-full flex p-2 text-zinc-800'>
+
+            <div className=' flex flex-col'>
+              <img className='w-[48px] h-[48px]'></img>
+              <div className=' border'>
+                <h2>Front-end developer</h2>
+                <h1>Ted Frosty</h1>
+              </div>
+              <div className='flex border-2 justify-start gap-2'>
+                <Twitter />
+                <Github />
+                <Linkedin />
+              </div>
+            </div>
+
+            <div>
+
+            </div>
+
+          </div>
         </div>
 
       </div >
