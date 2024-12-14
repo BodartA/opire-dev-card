f03832f578cdefafc7660d2e366c930b11b47b3d
BodartA <a.bodart23@gmail.com>
2024-12-14 19:26:19 +0100

    Right Part partially over

--- src/components/DevCard.tsx
+++ src/components/DevCard.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Twitter, Github, Linkedin } from 'lucide-react'
+import Barcode from '../assets/Barcode.png'
 
 const DevCard = () => {
   return (
@@ -13,24 +14,30 @@ const DevCard = () => {
 
         </div>
 
+
+
+
         <div className=' flex h-full w-[200px] bg-zinc-50 rounded-r-3xl rounded-l-lg'>
 
-          <div className=' h-full w-full flex p-2 text-zinc-800'>
+          <div className='flex flex-row p-4 text-zinc-800'>
 
-            <div className=' flex flex-col'>
-              <img className='w-[48px] h-[48px]'></img>
-              <div className=' border'>
-                <h2>Front-end developer</h2>
-                <h1>Ted Frosty</h1>
+            <div className=' flex flex-col w-1/2 gap-1'>
+              <div className='[writing-mode:vertical-rl] flex-1 flex flex-col justify-end overflow-hidden text-ellipsis'>
+                <h2 className=' text-emerald-600 font-light text-xs line-clamp-2'>Frontend developer</h2>
+                <h1 className=' text-xl font-bold line-clamp-2'>Antoine BODART</h1>
               </div>
-              <div className='flex border-2 justify-start gap-2'>
-                <Twitter />
-                <Github />
-                <Linkedin />
+              <div className='flex justify-start gap-2'>
+                <Twitter size={20} />
+                <Github size={20} />
+                <Linkedin size={20} />
               </div>
             </div>
 
-            <div>
+            <div className='flex flex-row w-1/2 gap-2'>
+              <div className=' text-center [writing-mode:vertical-lr] font-semibold text-xs font-mono'>Joined : 02/20/23</div>
+              <div className=' flex flex-1 justify-center items-start'>
+                <img className=' h-full w-full object-cover' src={Barcode}></img>
+              </div>
 
             </div>
 
