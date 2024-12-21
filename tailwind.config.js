3e3c27340fd1aeff7b9418aa07ef2460836c39f6
BodartA <a.bodart23@gmail.com>
2024-12-21 17:52:34 +0100

    Infinite scroll time change

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ export default {
   theme: {
     extend: {
       animation: {
-        'infinite-scroll': 'infinite-scroll 25s linear infinite',
+        'infinite-scroll': 'infinite-scroll 20s linear infinite',
       },
       keyframes: {
         'infinite-scroll': {
